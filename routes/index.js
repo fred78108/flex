578eef104c6bf89246e80af707f317e44b11b17b
Fred Diehl <fred78108@gmail.com>
2018-11-03 11:58:38 -0500

    updated header

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to the Awesomeness!! aint it great'});
+  res.render('index', { title: 'Welcome to the Awesomeness!!'});
 });
 
 module.exports = router;
