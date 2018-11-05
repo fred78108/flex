e93e9dd287ca0c171c8344210ed0fb3d88850197
Fred Diehl <fred78108@gmail.com>
2018-11-05 12:11:06 -0600

    added a /pop to the agent

--- routes/agent.js
+++ routes/agent.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to the Agent!!'});
 });
 
+router.get('/pop/:callerId',function(req,res){    
+  res.render('caller', {caller: req.params.callerId});
+});
+
+router.get('/test', function(req,res){
+  res.render('index', {title: 'you passed!'});
+});
+
 module.exports = router;
